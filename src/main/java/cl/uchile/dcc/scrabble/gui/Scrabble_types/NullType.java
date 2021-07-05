7435e23fc8dd99559eb55322e5910761019dc447
Joportus <81198811+Joportus@users.noreply.github.com>
2021-07-05 02:36:50 -0400

    primera entrega

--- src/main/java/cl/uchile/dcc/scrabble/gui/Scrabble_types/NullType.java
+++ src/main/java/cl/uchile/dcc/scrabble/gui/Scrabble_types/NullType.java
@@ -1,7 +1,16 @@
 package cl.uchile.dcc.scrabble.gui.Scrabble_types;
 
+
+/**
+ * This class represents a NullType object.
+ * represents a null.
+ */
 public class NullType extends Type {
 
+    /**
+     * Receives an object as an input, if its an instanceof NullType
+     * returns true, otherwise it returns false.
+     */
     @Override
     public boolean equals(Object obj) {
         return obj instanceof NullType;
