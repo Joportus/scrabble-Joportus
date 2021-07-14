a29672bdd400493118cbd739ca6d2d5cac225435
Joportus <81198811+Joportus@users.noreply.github.com>
2021-07-13 20:41:38 -0400

    cree visitors vacíos

---
+++ src/main/java/cl/uchile/dcc/scrabble/gui/FlowControl/IfVisitor.java
@@ -0,0 +1,13 @@
+package cl.uchile.dcc.scrabble.gui.FlowControl;
+
+import cl.uchile.dcc.scrabble.gui.Itypes;
+import cl.uchile.dcc.scrabble.gui.Soperations.treeNode;
+
+
+public class IfVisitor implements FlowControlVisitor{
+
+    @Override
+    public Itypes visitTreeNode(treeNode node) {
+        return null;
+    }
+}
